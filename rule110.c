8090b2e2930b2a94505900092dcef98530fc3317
ravi0li <29655971+r4v10l1@users.noreply.github.com>
2022-04-06 09:30:43 +0200

    v1.8

--- rule110.c
+++ rule110.c
@@ -6,34 +6,37 @@
 
 #include <stdio.h>
 
-#define STEPS 60000				// Amount of lines
-#define MAX_LINE_LEN 250		// Will jump to the next line if the line is too long
+#define STEPS 60000			// Amount of lines. Will be infinite if 0
+#define MAX_LINE_LEN 200		// Max length of the lines so they don't overflow
 
 #define FILLCHAR 219			// Character to display 1
 #define EMPTYCHAR ' '			// Character to display 0
 
-#define PRINT_ARRAY 1
-#define PRINT_DETAILS 0
+#define PRINT_DETAILS 1			// If 1 will print details about the current simulation
 
-int calc_rule(int a, int b, int c, int mode);	// Rule conditionals
-int test_rule(int);								// Debug
+int calc_rule(int a, int b, int c, int mode);		// Rule conditionals
+void print_line(int line_array[], int line_len);	// For printing the array to the console
 
 int main() {
-	//test_rule(1);
+	// We need to make it same as SPTEPS because it grows proportionally in width
+	int array_size = 10000;
+	if (STEPS) array_size = STEPS;
 
-	char line[STEPS+1] = {0, 1};  	// Array for line
-	char line2[STEPS+1] = {0, 1}; 	// Create another array to not overwrite the first one
-	int rule_results, change_line;
-	int position = 0;
+	int line[array_size], line_buffer[array_size];
+	int rule_results;
 	
+	// Set the values here so it doesn't throw an error
+	line[0] = 0;
+	line[1] = 1;
+
 	// Clear the arrays
-	for (int n = 2; n <= STEPS+1; n++){
+	for (int n = 2; n <= array_size; n++){
 		line[n] = 0;
-		line2[n] = 0;
+		line_buffer[n] = 0;
 	}
 
 	// Displaying
-	if (PRINT_DETAILS == 0) {
+	if (PRINT_DETAILS) {
 		printf("+------------------------------------+\n");
 		printf("| Rule 110 visualization             |\n");
 		printf("| https://github.com/r4v10l1/rule110 |\n");
@@ -46,84 +49,36 @@ int main() {
 		printf("\n");
 	}
 
-	// Display each line
-	for (int line_len = 1; line_len <= STEPS; line_len++){
-
-		// Print array for debug
-		if (PRINT_ARRAY == 0) {
-			for (int n = 0; n < STEPS; n++){
-				if (change_line == 1) {
-					printf("%d ", line[n]);
-				} else {
-					printf("%d ", line2[n]);
-				}
-			}
-			printf("\n");
-		}
-		
-		for (int i = 1; i <= line_len; i++){  // Each character
-			if (change_line == 1) {
-				rule_results = calc_rule(line[position], line[position+1], line[position+2], 1);
-			} else {
-				rule_results = calc_rule(line2[position], line2[position+1], line2[position+2], 1);
-			}
-
-			// Skip line checking if MAX_LINE_LEN is 0
-			if (MAX_LINE_LEN != 0) {
-				// Only print the character if the line is not too long
-				if (i <= MAX_LINE_LEN) {
-					if (rule_results == 0) {
-						putchar(EMPTYCHAR);
-					} else {
-						putchar(FILLCHAR);
-					}
-				}
-			} else {
-				if (rule_results == 0) {
-					putchar(EMPTYCHAR);
-				} else {
-					putchar(FILLCHAR);
-				}
-			}
-
-			// Switch lines
-			if (change_line == 0) {
-				line[i] = rule_results;
-			} else {
-				line2[i] = rule_results;
-			}
-
-			position++;
+	for (int line_n = 1; line_n <= STEPS || STEPS == 0; line_n++){		// Each line
+		for (int char_pos = 1; char_pos <= line_n; char_pos++){		// Each character to be printed
+			// The loop needs to start with 1 because we subtract one,
+			// and the '-1' is 0 (see array declaration)
+			rule_results = calc_rule(line[char_pos-1], line[char_pos], line[char_pos+1], 1);
+			line_buffer[char_pos] = rule_results;
 		}
 
-		printf("\n");
-		position = 0;
-
-		if (change_line == 0) {
-			change_line = 1;
-		} else {
-			change_line = 0;
+		// Move buffer to original array
+		for (int n = 0; n < array_size; n++) {
+			line[n] = line_buffer[n];
 		}
 
+		print_line(line, line_n);
 	}
 
 	return 0;
-}  
-  
-int test_rule(int option) {
-	// Option 0 - Test calc_rule(int, int, int, 0)
-	// Option 1 - Test calc_rule(int, int, int, 1)
-	printf("Testing 000: %d\n", calc_rule(0, 0, 0, option));
-	printf("Testing 001: %d\n", calc_rule(0, 0, 1, option));
-	printf("Testing 010: %d\n", calc_rule(0, 1, 0, option));
-	printf("Testing 011: %d\n", calc_rule(0, 1, 1, option));
-	printf("Testing 100: %d\n", calc_rule(1, 0, 0, option));
-	printf("Testing 101: %d\n", calc_rule(1, 0, 1, option));
-	printf("Testing 110: %d\n", calc_rule(1, 1, 0, option));
-	printf("Testing 111: %d\n", calc_rule(1, 1, 1, option));	
-	printf("Testing 312: %d\n", calc_rule(3, 1, 2, option));
-	printf("----------------\n");
-	return 0;
+}
+
+void print_line(int line[], int line_len) {
+	for (int char_pos = 1; char_pos <= line_len; char_pos++){	// Each character to be printed
+		if (MAX_LINE_LEN && char_pos <= MAX_LINE_LEN) {		// Skip line checking if MAX_LINE_LEN is 0
+			if (line[char_pos]) putchar(EMPTYCHAR);
+			else putchar(FILLCHAR);
+		} else {
+			if (line[char_pos]) putchar(EMPTYCHAR);
+			else putchar(FILLCHAR);
+		}
+	}
+	printf("\n");
 }
 
 int calc_rule(int a, int b, int c, int mode) {
