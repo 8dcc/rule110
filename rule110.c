f37dffd13bc6ca03367872f97ecb1004f2ad4f9d
8wiw <108031058+8wiw@users.noreply.github.com>
2022-06-22 23:29:09 +0200

    Fix indentation and format last conditionals

--- rule110.c
+++ rule110.c
@@ -18,95 +18,85 @@ int calc_rule(int a, int b, int c, int mode);		// Rule conditionals
 void print_line(int line_array[], int line_len);	// For printing the array to the console
 
 int main() {
-	// We need to make it same as SPTEPS because it grows proportionally in width
-	int array_size = 10000;
-	if (STEPS) array_size = STEPS;
+    // We need to make it same as SPTEPS because it grows proportionally in width
+    int array_size = 10000;
+    if (STEPS) array_size = STEPS;
 
-	int line[array_size], line_buffer[array_size];
-	int rule_results;
-	
-	// Set the values here so it doesn't throw an error
-	line[0] = 0;
-	line[1] = 1;
+    int line[array_size], line_buffer[array_size];
+    int rule_results;
 
-	// Clear the arrays
-	for (int n = 2; n <= array_size; n++){
-		line[n] = 0;
-		line_buffer[n] = 0;
-	}
+    // Set the values here so it doesn't throw an error
+    line[0] = 0;
+    line[1] = 1;
 
-	// Displaying
-	if (PRINT_DETAILS) {
-		printf("+------------------------------------+\n");
-		printf("| Rule 110 visualization             |\n");
-		printf("| https://github.com/r4v10l1/rule110 |\n");
-		printf("+------------------------------------+\n");
-		printf("| Current settings:                  |\n");
-		printf("+---------------+--------------------+\n");
-		printf("| STEPS:  %5d | MAX_LINE_LEN: %3d  |\n", STEPS, MAX_LINE_LEN);
-		printf("| FILLCHAR: %3d | EMPTYCHAR: %3d     |\n", FILLCHAR, EMPTYCHAR);
-		printf("+---------------+--------------------+\n");
-		printf("\n");
-	}
+    // Clear the arrays
+    for (int n = 2; n <= array_size; n++){
+        line[n] = 0;
+        line_buffer[n] = 0;
+    }
 
-	for (int line_n = 1; line_n <= STEPS || STEPS == 0; line_n++){		// Each line
-		for (int char_pos = 1; char_pos <= line_n; char_pos++){		// Each character to be printed
-			// The loop needs to start with 1 because we subtract one,
-			// and the '-1' is 0 (see array declaration)
-			rule_results = calc_rule(line[char_pos-1], line[char_pos], line[char_pos+1], 1);
-			line_buffer[char_pos] = rule_results;
-		}
+    // Displaying
+    if (PRINT_DETAILS) {
+        printf("+------------------------------------+\n");
+        printf("| Rule 110 visualization             |\n");
+        printf("| https://github.com/r4v10l1/rule110 |\n");
+        printf("+------------------------------------+\n");
+        printf("| Current settings:                  |\n");
+        printf("+---------------+--------------------+\n");
+        printf("| STEPS:  %5d | MAX_LINE_LEN: %3d  |\n", STEPS, MAX_LINE_LEN);
+        printf("| FILLCHAR: %3d | EMPTYCHAR: %3d     |\n", FILLCHAR, EMPTYCHAR);
+        printf("+---------------+--------------------+\n");
+        printf("\n");
+    }
 
-		// Move buffer to original array
-		for (int n = 0; n < array_size; n++) {
-			line[n] = line_buffer[n];
-		}
+    for (int line_n = 1; line_n <= STEPS || STEPS == 0; line_n++){		// Each line
+        for (int char_pos = 1; char_pos <= line_n; char_pos++){		// Each character to be printed
+            // The loop needs to start with 1 because we subtract one,
+            // and the '-1' is 0 (see array declaration)
+            rule_results = calc_rule(line[char_pos-1], line[char_pos], line[char_pos+1], 1);
+            line_buffer[char_pos] = rule_results;
+        }
 
-		print_line(line, line_n);
-	}
+        // Move buffer to original array
+        for (int n = 0; n < array_size; n++) {
+            line[n] = line_buffer[n];
+        }
 
-	return 0;
+        print_line(line, line_n);
+    }
+
+    return 0;
 }
 
 void print_line(int line[], int line_len) {
-	for (int char_pos = 1; char_pos <= line_len; char_pos++){	// Each character to be printed
-		if (MAX_LINE_LEN && char_pos <= MAX_LINE_LEN) {		// Skip line checking if MAX_LINE_LEN is 0
-			if (line[char_pos]) putchar(EMPTYCHAR);
-			else putchar(FILLCHAR);
-		} else {
-			if (line[char_pos]) putchar(EMPTYCHAR);
-			else putchar(FILLCHAR);
-		}
-	}
-	printf("\n");
+    for (int char_pos = 1; char_pos <= line_len; char_pos++){	// Each character to be printed
+        if (MAX_LINE_LEN && char_pos <= MAX_LINE_LEN) {		// Skip line checking if MAX_LINE_LEN is 0
+            if (line[char_pos]) putchar(EMPTYCHAR);
+            else putchar(FILLCHAR);
+        } else {
+            if (line[char_pos]) putchar(EMPTYCHAR);
+            else putchar(FILLCHAR);
+        }
+    }
+    printf("\n");
 }
 
 int calc_rule(int a, int b, int c, int mode) {
-	// Vanilla rule110 or mirror
-	if (mode == 1) {
-		int temp = a;
-		a = c;
-		c = temp;
-	}
+    // Vanilla rule110 or mirror
+    if (mode == 1) {
+        int temp = a;
+        a = c;
+        c = temp;
+    }
 
-	// Made a conditional for each rule so it's more visual
-	if (a == 0 && b == 0 && c == 0) {
-		return 0;	// 000
-	} else if (a == 0 && b == 0 && c == 1) {
-		return 1;	// 001
-	} else if (a == 0 && b == 1 && c == 0) {
-		return 1;	// 010
-	} else if (a == 0 && b == 1 && c == 1) {
-		return 1;	// 011
-	} else if (a == 1 && b == 0 && c == 0) {
-		return 0;	// 100
-	} else if (a == 1 && b == 0 && c == 1) {
-		return 1;	// 101
-	} else if (a == 1 && b == 1 && c == 0) {
-		return 1;	// 110
-	} else if (a == 1 && b == 1 && c == 1) {
-		return 0;	// 111
-	} else {
-		return 2;	// ???
-	}
+    // Made a conditional for each rule so it's more visual
+    if (a == 0 && b == 0 && c == 0)         return 0;	// 000
+    else if (a == 0 && b == 0 && c == 1)    return 1;	// 001
+    else if (a == 0 && b == 1 && c == 0)    return 1;	// 010
+    else if (a == 0 && b == 1 && c == 1)    return 1;	// 011
+    else if (a == 1 && b == 0 && c == 0)    return 0;	// 100
+    else if (a == 1 && b == 0 && c == 1)    return 1;	// 101
+    else if (a == 1 && b == 1 && c == 0)    return 1;	// 110
+    else if (a == 1 && b == 1 && c == 1)    return 0;	// 111
+    else                                    return 2;	// ???
 }
